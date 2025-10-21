9de303801b8a1dfedea5c662221d723fc8a1bbb1
alex19l <j.alexander.lopez.f@gmail.com>
2025-10-21 10:35:15 -0500

    clase 2025-10-21

--- modulo2/javascript/05_bucles.js
+++ modulo2/javascript/05_bucles.js
@@ -7,3 +7,17 @@ for (let i = 1; i <= 10; i++){
     suma += i;
 }
 console.log("Sumatoria indices", suma)
+
+let nombre = "Alexander";
+for (let i = 0; i <= nombre.lengrh; i++){
+    console,log(nombre[i])
+}
+
+
+// Ejercicio en clase.
+tabla4 = 4
+for (let i = 4; i <= 4; i++){
+    console.log("numero", i)
+}
+console.log("La tabla del 4 es:", tabla4)
+//
